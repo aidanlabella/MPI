1b15de804bcf3b25aeeb21347bc68190a7e30f48
aidanlabella <apl1341@rit.edu>
2021-12-01 11:30:08 -0500

    added test code

--- Collective.cs
+++ Collective.cs
@@ -1,29 +1,30 @@
 using System;
+using System.Collections.Generic;
 
 namespace mpi {
     public static class Collective {
 
         public static void ReduceAll<T>(ref T toBeReduced, MPI.ReduceFunc<T> f) {
-            Console.WriteLine("Je me souviens");
+            MPI.Reduce<T>(0, ref toBeReduced, f);
+
+            //Broadcast here...
+            Console.WriteLine("je me souviens");
         }
 
         public static List<T> Gather<T>(long toWhere, List<T> toBeGathered) {
 
+            return null;
         }
 
         public static List<T> GatherAll<T>(List<T> toBeGathered) {
 
+            return null;
         }
 
         public static List<T> Scatter<T>(long fromWhere, List<T> toScatter) {
-
+            Console.WriteLine("je me souviens");
+            
+            return null;
         }
-
-        //Used for testing
-        // public static void Main(string [] args) {
-        //     Console.WriteLine("What");
-        //     Console.Beep();
-        // }
-
     }
 }
